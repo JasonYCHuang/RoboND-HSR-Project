6216bed40618b9fc8d73bba2365cd5e034a6abac
jasonych99 <jasonych99@gmail.com>
2018-08-05 21:05:03 +0200

    Change gmapping Launch files with params

--- src/ShellScripts/wall_follower.sh
+++ src/ShellScripts/wall_follower.sh
@@ -4,7 +4,7 @@ export TURTLEBOT_GAZEBO_WORLD_FILE=$(rospack find World)/u_home.world
 
 xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm  -e  "roslaunch turtlebot_gazebo gmapping_demo.launch" & 
+xterm  -e  "roslaunch Launch gmapping.launch" &
 sleep 5
 xterm  -e  "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
